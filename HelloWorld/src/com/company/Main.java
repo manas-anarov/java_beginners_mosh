1e1cc66c8878435175ab960de5810a5203e0e61e
manas-anarov <you@example.com>
2020-07-22 14:35:26 +0600

    31

---
+++ HelloWorld/src/com/company/Main.java
@@ -0,0 +1,25 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+//        int age = 30;
+////        age = 35;
+
+//        int myAge = 30;
+//        int herAge =myAge;
+//       System.out.println(herAge);
+
+//        byte age = 30;
+//        long viewsCount = 3_123_456_789L;
+//        float price = 10.99F;
+//        char letter = 'A';
+//        boolean isEligible = false;
+
+        byte age = 30;
+        long viewsCount = 3_123_456_789L;
+
+
+
+    }
+}
