3f729fa6dad5c7537ecb7d3fe0bfd202e21f8ce3
manas-anarov <you@example.com>
2020-07-22 15:12:28 +0600

    45

--- HelloWorld/src/com/company/Main.java
+++ HelloWorld/src/com/company/Main.java
@@ -1,14 +1,13 @@
 package com.company;
 
+import java.awt.*;
+import java.util.Date;
+
 public class Main {
 
     public static void main(String[] args) {
-//        int age = 30;
-////        age = 35;
 
-//        int myAge = 30;
-//        int herAge =myAge;
-//       System.out.println(herAge);
+//45 minute
 
 //        byte age = 30;
 //        long viewsCount = 3_123_456_789L;
@@ -16,9 +15,30 @@ public class Main {
 //        char letter = 'A';
 //        boolean isEligible = false;
 
-        byte age = 30;
-        long viewsCount = 3_123_456_789L;
 
+
+//        byte age = 30;
+//        Date now = new Date();
+//        now.getTime();
+//        System.out.println(now);
+
+//        byte x = 1;
+//        byte y = x;
+//        x = 2;
+//        System.out.println(y);
+
+
+
+//        Point point1 = new Point(1,1);
+//        Point point2 = point1;
+//
+//        point1.x =2;
+//        System.out.println(point2);
+
+
+//        String message =  new String("Hello World");
+        String message =  "Hello World";
+        System.out.println(message);
 
 
     }
